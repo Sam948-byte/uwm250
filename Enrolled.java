550d5c793701136c7918f6117c5cb7d1a02ff54e
Sam <techguy763@gmail.com>
2023-09-11 18:13:08 +0000

    dang this is kinda broing

---
+++ Enrolled.java
@@ -0,0 +1,10 @@
+public class Enrolled {
+    public static void main(String[] args) {
+        System.out.println("COMPSCI 250: Introductory Computer Programming (3 credits)");
+        System.out.println("402 MoWe 12:30PM - 1:20PM");
+        System.out.println("Lapham Hall 160");
+        System.out.println(
+                "I only have one class in my schedule because I'm a dual enrollment student, just figured I'd give a heads up");
+
+    }
+}
