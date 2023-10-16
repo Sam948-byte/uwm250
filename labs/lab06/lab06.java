0ea31f9ecafd31b86ca11c9bf740503414c3537d
Sam948-byte <techguy763@gmail.com>
2023-10-16 12:28:13 -0500

    add validation util

--- labs/lab06/lab06.java
+++ labs/lab06/lab06.java
@@ -6,27 +6,24 @@ This program acts as a cash register for an eyeglass store
 
 package lab06;
 
-import utils.FastReader;
-
 public class lab06 {
 
-    public static final double[][] cost = {{52.50, 49.99}, {37.50, 34.99}};
+    //list of costs of glasses
+    public static final double[][] cost = { { 52.50, 49.99 }, { 37.50, 34.99 } };
+
     public static void main(String[] args) {
-        FastReader in = new FastReader();
 
+        //solicit and validate type of glasses
         System.out.println("What kind of glasses would you like:");
-        int g;
-        do {
-            System.out.print("1 -> Prescription, 2 -> Non-Prescription: ");
+        int g = Integer.parseInt(utils.Validator.getValidatedInput(
+                "1 -> Prescription, 2 -> Non-Prescription: ", "[12]")) - 1;
 
-        } while ((g = in.nextInt()) != 1 && g != 2);
-
+        //solicit and validate type of coating
         System.out.println("What kind of coating would you like:");
-        int c;
-        do {
-            System.out.print("1 -> Anti-Glare, 2 -> Brown-Tint: ");
-        } while ((c = in.nextInt()) != 1 && c != 2);
+        int c = Integer.parseInt(utils.Validator.getValidatedInput(
+                "1 -> Anti-Glare, 2 -> Brown-Tint: ", "[12]")) - 1;
 
-        System.out.println("Your cost is $" + cost[g-1][c-1] + ".");
+        //print cost
+        System.out.println("Your cost is $" + cost[g][c] + ".");
     }
 }
