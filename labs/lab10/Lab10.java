77a039f8564700d7954b43ffa64638734fc9dbd1
Sam <techguy763@gmail.com>
2023-11-27 04:18:28 +0000

    add sentinal terminator

--- labs/lab10/Lab10.java
+++ labs/lab10/Lab10.java
@@ -44,6 +44,7 @@ public class Lab10 {
 
     public static void doubArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) break;
             arr[i] *= 2;
         }
     }
