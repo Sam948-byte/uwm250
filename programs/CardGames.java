edb7ee29da662653f8752d63e9efa1a4bce41ed5
Sam <techguy763@gmail.com>
2023-11-28 00:28:34 +0000

    finish cardGames

---
+++ programs/CardGames.java
@@ -0,0 +1,69 @@
+import utils.RandomGenerator;
+
+public class CardGames {
+    public static final String[] suits = { "Club", "Spade", "Heart", "Diamond" };
+
+    public static class Card {
+        private int num;
+
+        public Card(int num) {
+            this.num = num;
+        }
+
+        public int cardValue() {
+            return num % 9 + 1;
+        }
+
+        public String cardSuit() {
+            return suits[num / 9];
+        }
+
+        public void displayCard() {
+            System.out.println(+ cardValue() + " of " + cardSuit() + "s");
+        }
+    }
+
+    public static void main(String[] args) {
+        Card[] deck = generateDeck();
+
+        int n = RandomGenerator.generate(0, 35);
+        System.out.println(n + 1);
+        deck[n].displayCard();
+
+        displayDeck(deck);
+
+        System.out.println("Shuffling");
+        shuffleDeck(deck, 50);
+        displayDeck(deck);
+
+    }
+
+    public static Card[] generateDeck() {
+        Card[] deck = new Card[36];
+
+        for (int i = 0; i < deck.length; i++) {
+            deck[i] = new Card(i);
+        }
+
+        return deck;
+    }
+
+    public static void displayDeck(Card[] deck) {
+        for (Card card : deck) {
+            card.displayCard();
+        }
+    }
+
+    public static void shuffleDeck(Card[] deck, int n) {
+        Card temp;
+
+        for (int i = 0; i < n; i++) {
+            int j = RandomGenerator.generate(0, 35);
+            int k = RandomGenerator.generate(0, 35);
+            temp = deck[j];
+            deck[j] = deck[k];
+            deck[k] = temp;
+        }
+
+    }
+}
